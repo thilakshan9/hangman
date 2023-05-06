17673f8a15e336867a55a626e062b0c69a14d19c
Thilakshan <thilakshanbalasubramaniam9@gmail.com>
2023-05-06 23:38:55 +0100

    Implements a conditional check to ensure that only a single letter is entered and only alphabetical characters are provided

--- milestone_2.py
+++ milestone_2.py
@@ -3,4 +3,9 @@ word_list = ["apple", "banana", "orange", "passionfruit", "guava"]
 print(word_list)
 word = random.choice(word_list)
 print(word)
-guess = input("Enter a single letter")
+guess = input("Enter a single letter ")
+
+if len(guess) == 1 and guess.isalpha() == True:
+    print("Good guess!")
+else:
+    print("Oops! That is not a valid input.")
