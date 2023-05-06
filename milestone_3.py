da0a9a0f2b235fae23fc223686f960f60c64b71b
Thilakshan <thilakshanbalasubramaniam9@gmail.com>
2023-05-07 00:17:52 +0100

    Makes check_guess and ask_for_input function, which contain previous code

--- milestone_3.py
+++ milestone_3.py
@@ -3,14 +3,22 @@ import random
 word_list = ["apple", "banana", "orange", "passionfruit", "guava"]
 word = random.choice(word_list)
 
-while True:
-    guess = input("Enter a single letter ")
-    if len(guess) == 1 and guess.isalpha() == True:
-        break
+def check_guess(guess):
+    guess = guess.lower()
+    if guess in word:
+        print(f"Good guess! {guess} is in the word.")
     else:
-        print("Invalid letter. Please, enter a single alphabetical character.")
+        print(f"Sorry {guess} is not in the word. Try again.")
+    
+def ask_for_input():
+    while True:
+        guess = input("Enter a single letter ")
+        if len(guess) == 1 and guess.isalpha() == True:
+            break
+        else:
+            print("Invalid letter. Please, enter a single alphabetical character.")
+    check_guess(guess)
 
-if guess in word:
-    print(f"Good guess! {guess} is in the word.")
-else:
-    print(f"Sorry {guess} is not in the word. Try again.")
+ask_for_input()
+
+
